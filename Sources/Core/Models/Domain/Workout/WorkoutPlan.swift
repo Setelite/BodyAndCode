69cd85a473eab0f7a1b107068c3bb9628fecaac5
Максим Анатольевич <gma150815@gmail.com>
2026-01-08 14:07:31 +0500

    New Target

--- Sources/Core/Models/Domain/Workout/WorkoutPlan.swift
+++ Sources/Core/Models/Domain/Workout/WorkoutPlan.swift
@@ -7,22 +7,27 @@
 
 import Foundation
 
+
+
 struct WorkoutPlan: Identifiable, Codable, Hashable {
+
     let id: UUID
     let name: String
     let description: String?
     let dayOfWeek: DayOfWeek
     let exercises: [Exercise]
-    let assignedTo: [UUID] // User IDs
+    let assignedTo: [UUID]
     let createdAt: Date
-    
-    init(id: UUID = UUID(),
-         name: String,
-         description: String? = nil,
-         dayOfWeek: DayOfWeek,
-         exercises: [Exercise],
-         assignedTo: [UUID],
-         createdAt: Date = Date()) {
+
+    init(
+        id: UUID = UUID(),
+        name: String,
+        description: String? = nil,
+        dayOfWeek: DayOfWeek,
+        exercises: [Exercise] = [],
+        assignedTo: [UUID] = [],
+        createdAt: Date = Date()
+    ) {
         self.id = id
         self.name = name
         self.description = description
@@ -33,12 +38,16 @@ struct WorkoutPlan: Identifiable, Codable, Hashable {
     }
 }
 
-enum DayOfWeek: String, CaseIterable, Codable {
-    case monday = "Monday"
-    case tuesday = "Tuesday"
-    case wednesday = "Wednesday"
-    case thursday = "Thursday"
-    case friday = "Friday"
-    case saturday = "Saturday"
-    case sunday = "Sunday"
+enum DayOfWeek: String, CaseIterable, Codable, Hashable {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+    case sunday
+
+    var displayName: String {
+        rawValue.capitalized
+    }
 }
